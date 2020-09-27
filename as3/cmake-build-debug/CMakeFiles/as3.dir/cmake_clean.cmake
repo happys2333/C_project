7f3ac6c01a6ed0371aedfb52bbf0dc3818e82e2d
happys <happys2333@outlook.com>
2020-09-27 22:52:38 +0800

    AS3

---
+++ as3/cmake-build-debug/CMakeFiles/as3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/as3.dir/main.cpp.o"
+  "as3"
+  "as3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/as3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
