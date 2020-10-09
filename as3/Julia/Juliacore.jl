252f761817d1f8b19b28304203eea8bb52a5e16f
happys <happys2333@outlook.com>
2020-10-09 10:38:01 +0800

    julia

--- as3/Julia/Juliacore.jl
+++ as3/Julia/Juliacore.jl
@@ -1,2 +1,8 @@
 print("Julia核心已经初始化完成，正在进入主程序")
-print("欢迎继续使用本程序，")
+print("欢迎继续使用本程序，输入-q退出程序，输入-f使用文件读取模式，输入-c进入命令行读取模式")
+cmd = readline()
+while cmd!="-q"
+     if cmd[0]!='-'
+         print("wrong command, please try again")
+     end
+end
