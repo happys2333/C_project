bfeef1f370818b204e7e0ff83b041cb2fd69c2fe
happys2333 <1013214271@qq.com>
2020-10-11 13:14:51 +0800

    cuda

---
+++ as3/normal/cmake-build-debug/CMakeFiles/normal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal.dir/main.cpp.obj"
+  "normal.exe"
+  "normal.exe.manifest"
+  "normal.lib"
+  "normal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
