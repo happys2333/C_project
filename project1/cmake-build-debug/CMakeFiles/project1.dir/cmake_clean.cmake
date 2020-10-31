47638159db5aaf73a991937d51ad6320a806d6fc
开心 <56923707+happys2333@users.noreply.github.com>
2020-10-31 17:25:46 +0800

    pro

---
+++ project1/cmake-build-debug/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/main.cpp.obj"
+  "libproject1.dll.a"
+  "project1.exe"
+  "project1.exe.manifest"
+  "project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
